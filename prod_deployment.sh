7528d093af142bdea080d3a54ccf58770716eb59
t-d-h <hoantd@coccoc.com>
2026-04-18 09:55:26 +0700

    1/2 of prod deployment

--- prod_deployment.sh
+++ prod_deployment.sh
@@ -1,1 +1,12 @@
 !#/bin/bash
+
+cd frontend; sudo docker build -t h1.dev.virt/wp-management-tool-frontend:latest .
+sudo docker push h1.dev.virt/wp-management-tool-frontend:latest
+
+cd ../backend; sudo docker build -t h1.dev.virt/wp-management-tool-backend:latest .
+sudo docker push h1.dev.virt/wp-management-tool-backend:latest
+
+cd ../worker; sudo docker build -t h1.dev.virt/wp-management-tool-worker:latest .
+sudo docker push h1.dev.virt/wp-management-tool-worker:latest
+
+ssh h1.dev.virt "cd /etc/docker-compose/wp-management-tool; docker compose down; docker compose pull; docker compose up -d"
